7c08489c8dbbe5730b2fd955a0b59db70e8bb92c
TheMichalLetkiewicz <michal.letkiewicz08.com>
2023-03-30 10:38:45 +0200

    .

--- rzymskie->normalne.py
+++ rzymskie->normalne.py
@@ -3,3 +3,6 @@
 #C = 100
 #D = 500
 #M = 1000
+liczba = int(input("Podaj liczbe arabska do zmienienia w rzymska: "))
+def rzysmkie_na_arabskie(liczba):  #to jednak arabskie na rzumskie
+    print("a")
