188e157d004953d20e7c3c392b52de4ec646d647
TheMichalLetkiewicz <michal.letkiewicz08.com>
2023-03-30 11:16:33 +0200

    .

--- miejsca-zerowe.py
+++ miejsca-zerowe.py
@@ -1,16 +1,22 @@
-jaka_fukcja = int(input("Podaj dla jakiej funkcji obliczyc miejsce zerowe(liniowa,kwadratowa): "))
+jaka_fukcja = (input("Podaj dla jakiej funkcji obliczyc miejsce zerowe(liniowa,kwadratowa): "))
 
 if jaka_fukcja == "liniowa": # y = ax + b
-    a=float(input("Podaj wspolczynnik kierunkowy: "))
-    b=float(input("Podaj wyraz wolny: "))
+    a=float(input("Podaj wspolczynnik kierunkowy a: "))
+    b=float(input("Podaj wyraz wolny b: "))
     if a == 0:
         if b == 0:
-            print("Funkcja posiada nieskończenie wiele rozwiązań.")
+            print("Funkcja posiada nieskończenie wiele miejsc zerowych.")
         else:
-            print("Funkcja nie posiada rozwiązań.")
+            print("Funkcja nie posiada miejsc zerowych.")
     else:
         x = -b/a
         print(x)
+elif jaka_fukcja == "kwadratowa": # y = ax**2 + bx + c
+    a=float(input("Podaj a funkcji kwadratowej: "))
+    b=float(input("Podaj b funkcji kwadratowej: ")) 
+    c=float(input("Podaj c funkcji kwadratowej: "))
+
+
 
     
 
