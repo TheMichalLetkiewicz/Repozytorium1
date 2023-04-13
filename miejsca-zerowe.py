97c61dbc0264dded25c8a52ea0c7cf58b7294492
TheMichalLetkiewicz <michal.letkiewicz08.com>
2023-04-13 11:00:16 +0200

    .

--- miejsca-zerowe.py
+++ miejsca-zerowe.py
@@ -14,8 +14,24 @@ if jaka_fukcja == "liniowa": # y = ax + b
 elif jaka_fukcja == "kwadratowa": # y = ax**2 + bx + c
     a=float(input("Podaj a funkcji kwadratowej: "))
     b=float(input("Podaj b funkcji kwadratowej: ")) 
-    c=float(input("Podaj c funkcji kwadratowej: "))
+    c=float(input("Podaj c funkcji kwadratowej: ")) #delta = b**2-4*a*c
+    delta = b**2-4*a*c
+    print("delta wynosi:",delta)
+    if a == 0:
+        if b == 0:
+            if c==0:
+                print("Funkcja posiada nieskończenie wiele miejsc zerowych.")
+    elif delta<0:
+        print("Funkcja nie posiada miejsc zerowych.")
+    elif delta>0:
+        mz1=(-b-delta**(1/2))/(2*a)
+        mz2=(-b+delta**(1/2))/(2*a)
+        print("Miejsca zerowe funkcji to:", mz1, mz2)        
+            
 
+        
+
+            
 
 
     
