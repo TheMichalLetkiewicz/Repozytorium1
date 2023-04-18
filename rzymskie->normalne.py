8f35df9a9ff2cfa92fbf2a76bf21aba8234c9e8e
TheMichalLetkiewicz <michal.letkiewicz08.com>
2023-04-18 11:00:45 +0200

    .

--- rzymskie->normalne.py
+++ rzymskie->normalne.py
@@ -4,8 +4,15 @@
 #D = 500
 #M = 1000
 
-#liczba = int(input("Podaj liczbe arabska do zmienienia w rzymska: "))
-#def rzysmkie_na_arabskie(liczba)
+jednosci = ["", "I", "II", "III", "IV", "V", "VI", "VII", "IIX", "IX"]
+dziesiatki = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "XXC", "CX"]
+setki = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "CCM", "CM"]
+
+
+arab = input("podaj liczbę")
+while len(arab) < 4:
+	arab = "0" + arab
+print (int(arab[-4]) * "M" + setki[int(arab[-3])] + dziesiatki[int(arab[-2])] + jednosci[int(arab[-1])])
 
 
 
