c9f0dedb19ae71bdf33ff2078ab1a3772c55d21a
TheMichalLetkiewicz <michal.letkiewicz08.com>
2023-04-27 11:06:46 +0200

    .

--- generator-hasel.py
+++ generator-hasel.py
@@ -3,11 +3,13 @@ import random
 
 ile = random.randint(8,16) 
 
+
 haslo = ""
-znaki = "abcdefghijklmnopqrstuwvxyzABCDEFGHIJKLMNOPRSTUVWXYZ1234567890!@$%^&*"
+znaki = "abcdefghijklmnopqrstuwvxyzABCDEFGHIJKLMNOPRSTUVWXYZ12345678901234567890!@$%^&*"
+
 for i in range(0,ile):
 	
-	haslo = str(haslo)+znaki[random.randint(0,67)]
+	haslo = str(haslo)+znaki[random.randint(0,77)]
 	
 print("---------------------------")
 print("Twoje hasło to: ", haslo)
---
+++ ta-gra-z-zapalkami-cala.py
@@ -0,0 +1,22 @@
+import turtle
+import random
+window = turtle.Screen()
+B = 675
+I = 1
+X = - 135
+Y= 135
+window.setup(B,B)
+window.title('Gra z zapałkami nie pamietam nazwy')
+window.bgcolor('green yellow')
+xo = turtle.Turtle()
+xo.color('green yellow')
+xo.speed(0)
+
+
+
+tablica=[[None, None, I, None, None]
+            [None, I, I, I, None]
+               [I, I, I, I, I]]
+
+
+
