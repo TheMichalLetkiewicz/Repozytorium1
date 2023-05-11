b65966afcd615f59d94251653da82319f9221ba5
TheMichalLetkiewicz <michal.letkiewicz08.com>
2023-05-11 11:18:59 +0200

    .

--- ta-gra-z-zapalkami-cala.py
+++ ta-gra-z-zapalkami-cala.py
@@ -1,25 +1,62 @@
 import turtle
 import random
 window = turtle.Screen()
-B = 705
 
-X = - 136
+B = 700
+
+X = - 135
 
 Y = 136
-Odstep = 235
+
+ODSTĘP = 136
+
 window.setup(B,B)
 window.title('Gra z zapałkami nie pamietam nazwy')
 window.bgcolor('green yellow')
-xo = turtle.Turtle()
-xo.color('green yellow')
-xo.speed(0)
+zolw = turtle.Turtle()
 
 
 
-tablica=[[None, None, None, I, None, None, None]
-            [None, None, I, I, I, None, None]
-               [None, I, I, I, I, I, None]
-                  [I, I, I, I, I, I, I]]
+tablica = [
+    [None, None, None, None, None, None, None],
+    [None, None, None, 'I', None, None, None],
+    [None, None, 'I', 'I', 'I', None, None],
+    [None, 'I', 'I', 'I', 'I', 'I', None],
+    ['I', 'I', 'I', 'I', 'I', 'I', 'I'],
+    [None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None]
+]
+
+
+
+
+
+
+
+
+
+
+
+
+zolw.width(1)
+zolw.speed(111000)
+square_size = window.window_width() / 7
+for i in range(0,7):
+    for j in range(0,7):
+        
+        zolw.penup()
+        zolw.goto(-window.window_width() / 2 + j * square_size,
+               window.window_height() / 2 - (i + 1) * square_size)
+        zolw.pendown()
+
+        
+        for k in range(4):
+            zolw.forward(square_size)
+            zolw.right(90)
+
+
+turtle.done()
+
 
 
 
